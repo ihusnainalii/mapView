f380a63cd40fb822fa93d7f0bb834537c5a87468
Henry Nguyen <nvqdung@gmail.com>
2018-12-17 13:19:17 -0500

    Getting an Address from a Coordinate using  Reverse Geolocating

--- mapView/ViewController.swift
+++ mapView/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
 
     let locationManager = CLLocationManager()
     let regionInMeters : Double = 10000
+    var previousLocation: CLLocation?
+    
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var imgPin: UIImageView!
+    @IBOutlet weak var lblAddress: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationService()
@@ -24,7 +28,7 @@ class ViewController: UIViewController {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
-    //Zoom in
+    //MARK: - Zoom in
     func centerViewOnUserLocation(){
         if let location = locationManager.location?.coordinate{
             let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
@@ -46,9 +50,7 @@ class ViewController: UIViewController {
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
             //Do Map Stuff
-            mapView.showsUserLocation = true
-            centerViewOnUserLocation()
-            break
+         startTrackingUserLocation()
         case .denied:
             // alert let them know hoe to turn on location permissions
             break
@@ -62,18 +64,60 @@ class ViewController: UIViewController {
             break
         }
     }
-
+    
+    func startTrackingUserLocation(){
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation{
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
 }
 
 extension ViewController: CLLocationManagerDelegate{
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        mapView.setRegion(region, animated: true)
-    }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
     checkLocationAuthorization()
+    }
+}
+
+extension ViewController: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        //guard to make sure previousLocation has value
+        guard let previousLocation = self.previousLocation else {return}
+        
+        guard center.distance(from: previousLocation) > 60 else {return}
+        self.previousLocation = center // assign oldLocation  to newLocation
+        
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else {return }
+            
+            if let _ = error{
+                // TODO: show the alert informing users
+                return
+            }
+            
+            guard let placemark = placemarks?.first  else {
+                //TODO: alert to user
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? "" // --> ?? "" means providing default value
+            let streetName = placemark.thoroughfare ?? ""
+            
+            // being inside the Closure --> Jump back to main thread
+            DispatchQueue.main.async {
+                self.lblAddress.text = "\(streetNumber) \(streetName)"
+            }
+        }
     }
 }
